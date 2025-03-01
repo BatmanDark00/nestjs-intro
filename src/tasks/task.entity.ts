93d72a78eb9730d817a2d29d77fa0f23370a7dd3
Francisco Picado <franciscopicado19@gmail.com>
2025-03-01 11:44:10 -0600

    feat: ✨ Update import paths for consistency and modify AppController to return a static message

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -7,7 +7,7 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { TaskStatus } from './task.model';
-import { User } from 'src/users/user.entity';
+import { User } from '../users/user.entity';
 import { TaskLabel } from './task.label.entity';
 
 @Entity()
