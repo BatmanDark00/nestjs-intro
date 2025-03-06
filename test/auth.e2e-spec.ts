5f146108b34a3ee9c745371dd5984a78aa1a4867
Francisco Picado <franciscopicado19@gmail.com>
2025-03-06 13:05:40 -0600

    feat: ✨ Refactor login response structure and enhance e2e tests for registration and login scenarios

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -34,4 +34,28 @@ describe('AppController (e2e)', () => {
     expect(response.body).toHaveProperty('name', testUser.name);
     expect(response.body).not.toHaveProperty('password');
   });
+
+  it('auth/register (POST) - email already exists', async () => {
+    await request(testSetup.app.getHttpServer())
+      .post('/auth/register')
+      .send(testUser);
+
+    return await request(testSetup.app.getHttpServer())
+      .post('/auth/register')
+      .send(testUser)
+      .expect(409);
+  });
+
+  it('/auth/login (POST)', async () => {
+    await request(testSetup.app.getHttpServer())
+      .post('/auth/register')
+      .send(testUser);
+
+    const response = await request(testSetup.app.getHttpServer())
+      .post('/auth/login')
+      .send({ email: testUser.email, password: testUser.password });
+
+    expect(response.status).toBe(201);
+    expect(response.body.accessToken).toBeDefined();
+  });
 });
